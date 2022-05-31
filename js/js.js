bd47f4bdefe445d5729ae8bd604bb861f0d681d4
valenby <30valep@gmail.com>
2022-05-31 16:48:12 +0200

    el codigo de snowboll

---
+++ js/js.js
@@ -0,0 +1,9 @@
+function mensaje () {
+   window.alert("esto es un mensaje") 
+}
+function mensaje2() {
+    window.confirm("esto es un mensaje de confirm") 
+}
+function mensaje3() {
+    window.prompt("digite su nombre") 
+}
